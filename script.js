a4af12f98d50db195f57f5a3a01c53be077e38b4
BrunoDias <brunophm@gmail.com>
2022-08-17 00:41:57 -0300

    requisito 21 implementação parcialmente funcional, apenas nome, email e casa são capturados

--- script.js
+++ script.js
@@ -41,40 +41,64 @@ function stateHandle() {
   console.log(checkbox.checked);
   if (!checkbox.checked) {
     buttonSend.disabled = true;
+    buttonSend.style.filter = 'brightness(30%)'; /* mantém o botão escurecido quando desativado */
   } else {
     buttonSend.disabled = false;
+    buttonSend.style.filter = 'brightness(100%)'; /* aumenta o brilho do botão quando ativado */
   }
 }
 checkbox.addEventListener('change', stateHandle);
 // Fim Requisito 18 - fonte: https://www.delftstack.com/pt/howto/javascript/javascript-disable-button/  ,  https://www.w3schools.com/jsref/prop_select_disabled.asp
 
+// Requisito 21
 
-//Requisito 21
+const formData = document.querySelectorAll('#form-data p');
+const evaluationForm = document.getElementById('evaluation-form');
 
-let formPrint = document.getElementById('form-data')
-
-
-function viewForm (){
-  let viewInput = document.getElementById('form-data');
-
-  let nomeInput = document.getElementById('input-name');
-  let sobrenomeInput = document.getElementById('input-lastname');
-  let emailInput = document.getElementById('input-email');
-  let casaInput = document.getElementById('house');
-  let familiaInput = document.getElementById('label-family');
-  let materiaInput = document.getElementById('container-conteudo');
-  let avaliacaoInput = document.getElementById('n-avaliacao');
-  let observacoesInput = document.getElementById('textarea');
-
-  viewInput.innerHTML = `Nome: ${nomeInput.value} ${sobrenomeInput.value}; 
-  E-mail: ${emailInput.value};
-  Casa: ${casaInput.value};
-  Família: ${familiaInput.value};
-  Materia: ${materiaInput.value};
-  Avaliação: ${avaliacaoInput.value};
-  Observações: ${observacoesInput.value}`;
-
+function checkSelection(input) {
+  for (let index = 0; index < input.length; index += 1) {
+    if (input[index].checked) {
+      return input[index];
+    }
+    return null;
+  }
 }
 
+function viewForm() {
+  const nomeInput = document.getElementById('input-name');
+  const sobrenomeInput = document.getElementById('input-lastname');
+  const emailInput = document.getElementById('input-email');
+  const casaInput = document.getElementById('house');
+  const familiaInput = document.querySelectorAll('#label-family input');
+  let materiaInput = document.querySelector('.container-conteudo');
+  materiaInput = checkSelection(materiaInput);
+  let avaliacaoInput = document.querySelectorAll('.avaliacao input');
+  avaliacaoInput = checkSelection(avaliacaoInput);
+  const observacoesInput = document.getElementById('textarea');
+  evaluationForm.style.display = 'none'; /* esconde o primeiro formulário quando o botão é clicado */
+  formData[0].innerText = `Nome: ${nomeInput.value} ${sobrenomeInput.value}`;
+  formData[1].innerText = `E-mail: ${emailInput.value}`;
+  formData[2].innerText = `Casa: ${casaInput.value}`;
+  formData[3].innerText = `Família: ${familiaInput.value}`;
+  formData[4].innerText = `Matérias: ${materiaInput.value}`;
+  formData[5].innerText = `Avaliação: ${avaliacaoInput.value}`;
+  formData[6].innerText = `Observações: ${observacoesInput.textContent}`;
+}
+// for (let index = 0; index < formData.length; index += 1) {
+  // const element = form[index];
+//  formData[index].innerText = `Nome: ${nomeInput} ${sobrenomeInput}`;
+// }
+// formData[0].innerText.style.color = 'red';
+// formData.style.display = 'initial';
+// form = [Nome, Email, Casa, Familia, Materias, Avaliação, Observações];
+// function modifyForm(form) {
+// }
+// formData.innerHTML = `Nome: ${nomeInput.value} ${sobrenomeInput.value};
+// E-mail: ${emailInput.value};
+// Casa: ${casaInput.value};
+// Família: ${familiaInput.value};
+// Matérias: ${materiaInput.value};
+// Avaliação: ${avaliacaoInput.value};
+// Observações: ${observacoesInput.value}`;
 
 buttonSend.addEventListener('click', viewForm);
